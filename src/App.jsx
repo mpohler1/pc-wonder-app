2e7336d3784ff6ec585f613aad024eb940f027c9
mpohler1 <masonpohler@gmail.com>
2020-04-20 17:05:14 -0500

    created component layout

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import SiteNavbar from "./components/SiteNavbar";
+import CategoryNavbar from "./components/CategoryNavbar";
+import MainView from "./components/MainView";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <SiteNavbar/>
+      </div>
+      <div className="row">
+        <div className="col">
+          <CategoryNavbar/>
+        </div>
+        <div className="col">
+          <MainView/>
+        </div>
+      </div>
+      <div className="row">
+        <Footer/>
+      </div>
+    </div>
+  );
+}
+
+export default App;

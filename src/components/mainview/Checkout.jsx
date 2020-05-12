1d7078452e4ac5124687dbbafbf31aaa7306d089
mpohler1 <masonpohler@gmail.com>
2020-05-12 07:54:52 -0500

    began writing JSX for Checkout

--- src/components/mainview/Checkout.jsx
+++ src/components/mainview/Checkout.jsx
@@ -4,10 +4,61 @@ class Checkout extends Component {
 
     render() {
         return (
-            <div>
-                <h1 className="display-1">
-                    Checkout
-                </h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <div className="form-row my-2">
+                            <div className="col">
+                                <label htmlFor="firstName">First Name</label>
+                                <input className="form-control" id="firstName" placeholder="First Name"/>
+                            </div>
+                            <div className="col">
+                                <label htmlFor="lastName">Last Name</label>
+                                <input className="form-control" id="lastName" placeholder="Last Name"/>
+                            </div>
+                        </div>
+                        <div className="form-row my-2">
+                            <div className="col">
+                                <label htmlFor="companyName">Company Name</label>
+                                <input className="form-control" id="companyName" placeholder="Company Name"/>
+                            </div>
+                        </div>
+                        <div className="form-row my-2">
+                            <div className="col-8">
+                                <label htmlFor="streetAddress">Street Address</label>
+                                <input className="form-control" id="streetAddress" placeholder="Street Address"/>
+                            </div>
+                            <div className="col-4">
+                                <label htmlFor="apartmentNumber">Apartment #</label>
+                                <input className="form-control" id="apartmentNumber" placeholder="Apartment #"/>
+                            </div>
+                        </div>
+                        <div className="form-row my-2">
+                            <div className="col-6">
+                                <label htmlFor="city">City</label>
+                                <input className="form-control" id="city" placeholder="City"/>
+                            </div>
+                            <div className="col-2">
+                                <label htmlFor="state">State</label>
+                                <input className="form-control" id="state" placeholder="State"/>
+                            </div>
+                            <div className="col-4">
+                                <label htmlFor="zip">Zip</label>
+                                <input className="form-control" id="zip" placeholder="Zip"/>
+                            </div>
+                        </div>
+                        <div className="form-row my-2">
+                            <div className="col-6">
+                                <label htmlFor="email">Email Address</label>
+                                <input className="form-control" id="email" placeholder="email@address.com"/>
+                            </div>
+                            <div className="col-6">
+                                <label htmlFor="phone">Phone Number</label>
+                                <input className="form-control" id="phone" placeholder="123-456-7890"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }

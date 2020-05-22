11ded38c40353f4791f315cf6e688226b877a39f
mpohler1 <masonpohler@gmail.com>
2020-05-21 19:08:05 -0500

    disabled eslint for line containing email regex

--- src/resources/constraints.js
+++ src/resources/constraints.js
@@ -26,6 +26,7 @@ export const CONSTRAINTS = {
     email: {
         format: {
             // Pattern was found on https://emailregex.com
+            // eslint-disable-next-line
             pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             message: "must be a valid email address"
         }

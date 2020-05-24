6dc6b4c150e54f6e4987b9aa3de5a6bb2e77536a
mpohler1 <masonpohler@gmail.com>
2020-05-23 23:52:29 -0500

    changed routes and added a redirect from / to /products

--- src/components/mainview/MainView.jsx
+++ src/components/mainview/MainView.jsx
@@ -3,21 +3,28 @@ import ProductGrid from "./productgrid/ProductGrid";
 import ProductDetail from "./productdetail/ProductDetail";
 import Cart from "./cart/Cart";
 import Checkout from "./checkout/Checkout";
-import {CART, CHECKOUT, ORDER_CONFIRMATION, PRODUCT_DETAIL, PRODUCT_GRID} from "../../resources/routes";
+import {
+    CART_SCHEME,
+    CHECKOUT_SCHEME,
+    ORDER_CONFIRMATION_SCHEME,
+    PRODUCT_DETAIL_SCHEME,
+    PRODUCT_GRID_SCHEME
+} from "../../resources/routes";
 import {connect} from "react-redux";
 import OrderConfirmation from "./orderconfirmation/OrderConfirmation";
-import {Route, Switch} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 
 class MainView extends Component {
 
     render() {
         return (
             <Switch>
-                <Route path={PRODUCT_GRID} exact component={ProductGrid}/>
-                <Route path={PRODUCT_DETAIL} component={ProductDetail}/>
-                <Route path={CART} component={Cart}/>
-                <Route path={CHECKOUT} component={Checkout}/>
-                <Route path={ORDER_CONFIRMATION} component={OrderConfirmation}/>
+                <Redirect exact from={"/"} to={PRODUCT_GRID_SCHEME}/>
+                <Route path={PRODUCT_GRID_SCHEME} exact component={ProductGrid}/>
+                <Route path={PRODUCT_DETAIL_SCHEME} component={ProductDetail}/>
+                <Route path={CART_SCHEME} component={Cart}/>
+                <Route path={CHECKOUT_SCHEME} component={Checkout}/>
+                <Route path={ORDER_CONFIRMATION_SCHEME} component={OrderConfirmation}/>
             </Switch>
         );
     }
--- src/resources/routes.js
+++ src/resources/routes.js
@@ -1,5 +1,11 @@
-export const PRODUCT_GRID = "/";
-export const PRODUCT_DETAIL = "/product/:uuid";
+export const PRODUCT_GRID_SCHEME = "/products/:categoryName?/:search?";
+export const PRODUCT_DETAIL_SCHEME = "/product/:uuid";
+export const CART_SCHEME = "/cart";
+export const CHECKOUT_SCHEME = "/checkout";
+export const ORDER_CONFIRMATION_SCHEME = "/order-confirmation";
+
+export const PRODUCT_GRID = "/products";
+export const PRODUCT_DETAIL = "/product/";
 export const CART = "/cart";
 export const CHECKOUT = "/checkout";
 export const ORDER_CONFIRMATION = "/order-confirmation";

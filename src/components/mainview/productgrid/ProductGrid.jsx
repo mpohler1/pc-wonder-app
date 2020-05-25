438ec9230ea00038f26e95e500de71fb5400f992
mpohler1 <masonpohler@gmail.com>
2020-05-25 16:36:51 -0500

    added JSX to ProductGrid that displays a message when a search results in an empty product list

--- src/components/mainview/productgrid/ProductGrid.jsx
+++ src/components/mainview/productgrid/ProductGrid.jsx
@@ -89,41 +89,59 @@ class ProductGrid extends Component {
 
     render() {
         return (
-            <div className="container-fluid">
-                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 bg-secondary pl-0 pr-2 pt-2 pb-0">
-                    {
-                        this.props.products.map(product => (
-                            <div className="col pr-0 pt-0 pl-2 pb-2 d-flex align-items-stretch">
-                                <div className="card container-fluid p-0 m-0">
-                                    <Link to={PRODUCT_DETAIL + product.uuid} onClick={() => this.handleProductClick(product)}>
-                                        <img src={product.imageURL}
-                                             className="btn btn-card-img-top card-img-top product-thumbnail container-fluid p-0 m-0"
-                                             alt={product.name + " image"}/>
-                                    </Link>
-                                    <div className="card-body">
-                                        <h5 className="card-title">
-                                            {product.name}
-                                        </h5>
-                                        <p className="card-subtitle">
-                                            {product.manufacturer.name}
-                                        </p>
-                                        <h5 className="card-title">
-                                            {product.rating}
-                                        </h5>
-                                        <h4 className="card-title">
-                                            ${product.price.toFixed(2)}
-                                        </h4>
-                                        <button className="btn btn-primary"
-                                                onClick={() => this.handleCartButtonClick(product)}>
-                                            Add To Cart
-                                        </button>
+            <React.Fragment>
+                {
+                    this.props.products.length === 0 &&
+                    <div className="container-fluid d-flex flex-row align-items-center page">
+                        <div className="container-fluid d-flex flex-column align-items-center">
+                            <h1 className="display-4">
+                                No products found.
+                            </h1>
+                            <p>
+                                No products could be found for the search query "{this.props.searchString}".
+                            </p>
+                        </div>
+                    </div>
+                }
+                {
+                    this.props.products.length > 0 &&
+                    <div className="container-fluid">
+                        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 bg-secondary pl-0 pr-2 pt-2 pb-0">
+                            {
+                                this.props.products.map(product => (
+                                    <div className="col pr-0 pt-0 pl-2 pb-2 d-flex align-items-stretch">
+                                        <div className="card container-fluid p-0 m-0">
+                                            <Link to={PRODUCT_DETAIL + product.uuid} onClick={() => this.handleProductClick(product)}>
+                                                <img src={product.imageURL}
+                                                     className="btn btn-card-img-top card-img-top product-thumbnail container-fluid p-0 m-0"
+                                                     alt={product.name + " image"}/>
+                                            </Link>
+                                            <div className="card-body">
+                                                <h5 className="card-title">
+                                                    {product.name}
+                                                </h5>
+                                                <p className="card-subtitle">
+                                                    {product.manufacturer.name}
+                                                </p>
+                                                <h5 className="card-title">
+                                                    {product.rating}
+                                                </h5>
+                                                <h4 className="card-title">
+                                                    ${product.price.toFixed(2)}
+                                                </h4>
+                                                <button className="btn btn-primary"
+                                                        onClick={() => this.handleCartButtonClick(product)}>
+                                                    Add To Cart
+                                                </button>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                        ))
-                    }
-                </div>
-            </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                }
+            </React.Fragment>
         );
     }
 }

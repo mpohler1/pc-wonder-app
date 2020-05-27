5ead5d95a13a8e4ebb121f8842927fe5867c901b
mpohler1 <masonpohler@gmail.com>
2020-05-27 18:58:25 -0500

    added privacy policy to the PrivacyPolicy page

--- src/components/mainview/privacypolicy/PrivacyPolicy.jsx
+++ src/components/mainview/privacypolicy/PrivacyPolicy.jsx
@@ -1,13 +1,32 @@
 import React, {Component} from "react";
+import {privacyPolicy} from "../../../resources/privacyPolicy";
 
 class PrivacyPolicy extends Component {
 
     render() {
         return (
-            <div className="container-fluid page">
-                <h1 className="display-1">
-                    Privacy Policy
-                </h1>
+            <div className="container-fluid">
+                <div className="row d-flex flex-row align-items-center page">
+                    <div className="container privacy-policy my-2">
+                        <div className="row mx-2">
+                            <h1>
+                                Privacy Policy
+                            </h1>
+                        </div>
+                        <div className="row mx-2 my-3">
+                            <p className="font-weight-light">
+                                This privacy policy applies to {privacyPolicy.siteURL} and is effective {privacyPolicy.effectiveDate}.
+                            </p>
+                        </div>
+                        {privacyPolicy.content.map(paragraph => (
+                            <div className="row mx-2 my-3">
+                                <p className="font-weight-light">
+                                    {paragraph}
+                                </p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         );
     }
---
+++ src/resources/privacyPolicy.js
@@ -0,0 +1,23 @@
+export const privacyPolicy = {
+    siteURL: "pc-wonder.masonpohler.com",
+    effectiveDate: "May 27th, 2020",
+    content: [
+        "This site uses an application state in order to function. This application state is stored in your " +
+        "computer's memory while you use this site. The application state contains any data created by the site " +
+        "itself and any data that you type into the forms on this site. This includes your name, physical address, " +
+        "and email address. This data is only used to properly display the pages on this site. It is not sent " +
+        "anywhere, and it does not persist when you leave the site. Cookies, databases, and other forms of storage " +
+        "are not used for user data on this site.",
+
+        "This site does not allow users to register accounts or purchase goods or services. This website serves as a " +
+        "demo for what an ecommerce website's user interface could be. This website does not actually function as a " +
+        "real ecommerce site. This means there is no real business behind PC Wonder, there is no infrastructure for " +
+        "accepting and processing payments, and there is no infrastructure for shipping products.",
+
+        "PC Wonder is expected to have future development cycles, in which registering accounts and using cookies " +
+        "will be implemented. This Privacy Policy will be updated to reflect these changes as they are implemented.",
+
+        "If you still have any questions or concerns about how your data is used, please contact Mason Pohler by " +
+        "email at dev@masonpohler.com"
+    ]
+};

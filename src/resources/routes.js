3a93a0541ec5a3a6ffd3857fb8c4e568188b5341
mpohler1 <masonpohler@gmail.com>
2020-06-03 01:16:49 -0500

    changed routing to work on gh pages

--- src/resources/routes.js
+++ src/resources/routes.js
@@ -1,16 +1,18 @@
-export const PRODUCT_GRID_SCHEME = "/products";
-export const PRODUCT_DETAIL_SCHEME = "/product/:uuid";
-export const CART_SCHEME = "/cart";
-export const CHECKOUT_SCHEME = "/checkout";
-export const ORDER_CONFIRMATION_SCHEME = "/order-confirmation";
-export const PRIVACY_POLICY_SCHEME = "/privacy-policy";
+export const BASE_URL = "/pc-wonder-app";
 
-export const PRODUCT_GRID = "/products";
-export const PRODUCT_DETAIL = "/product/";
-export const CART = "/cart";
-export const CHECKOUT = "/checkout";
-export const ORDER_CONFIRMATION = "/order-confirmation";
-export const PRIVACY_POLICY = "/privacy-policy";
+export const PRODUCT_GRID_SCHEME = BASE_URL + "/products";
+export const PRODUCT_DETAIL_SCHEME = BASE_URL + "/product/:uuid";
+export const CART_SCHEME = BASE_URL + "/cart";
+export const CHECKOUT_SCHEME = BASE_URL + "/checkout";
+export const ORDER_CONFIRMATION_SCHEME = BASE_URL + "/order-confirmation";
+export const PRIVACY_POLICY_SCHEME = BASE_URL + "/privacy-policy";
+
+export const PRODUCT_GRID = BASE_URL + "/products";
+export const PRODUCT_DETAIL = BASE_URL + "/product/";
+export const CART = BASE_URL + "/cart";
+export const CHECKOUT = BASE_URL + "/checkout";
+export const ORDER_CONFIRMATION = BASE_URL + "/order-confirmation";
+export const PRIVACY_POLICY = BASE_URL + "/privacy-policy";
 
 export const SEARCH_QUERY = "?search=";
 export const CATEGORY_QUERY = "?category=";
